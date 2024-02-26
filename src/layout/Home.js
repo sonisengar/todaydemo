88d3d18b7d60500a96304c39aabdcc63f262a917
soni sengar <soni7519kumari@gmail.com>
2024-02-26 13:21:31 +0530

    first commit

---
+++ src/layout/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div><h1>hello ducat india</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis obcaecati repellat ipsa. 
+        Obcaecati corporis dolore sed doloremque voluptatum illum sit impedit delectus aperiam in cupiditate
+         asperiores, ex, dolorum, dolores illo numquam. Inventore cum voluptatem quaerat optio vel laudantium 
+         reiciendis magni dolor, animi sapiente debitis illo sequi in beatae, voluptate saepe, a molestiae!
+          Earum reprehenderit accusamus quidem nisi numquam excepturi qui dolorem! Eaque, hic dolores. Laboriosam, 
+          quam atque cumque reprehenderit labore rerum neque placeat recusandae molestiae esse natus magnam, 
+          in incidunt, sapiente dolorum nisi illum. Provident optio, expedita quasi hic culpa commodi dicta accusantium, 
+          omnis deleniti quis nihil minus molestias eum!</p>
+        </div>
+
+  )
+}
+
+export default Home
